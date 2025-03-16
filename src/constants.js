e8a3dc449b04468c964bacd52eac5be8aee30506
Minato Hayashi <hayashiminatooo@gmail.com>
2025-03-16 14:46:24 -0400

    Baseurl fix

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://mvpbackend-1.onrender.com:5000/';
+export const BASE_URL = 'https://mvpbackend-1.onrender.com';
 export const USERS_URL = '/api/users';
 export const PROJECT_URL = '/api/project';
